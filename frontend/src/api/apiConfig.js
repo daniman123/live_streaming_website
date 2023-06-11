c20cf4eba6eed3855f9f63166b3ad103d38886b8
daniman123 <daniel-saleh@hotmail.com>
2023-06-12 00:04:21 +0200

    sssss

---
+++ frontend/src/api/apiConfig.js
@@ -0,0 +1,3 @@
+const API_URL = "https://api.example.com"; // Replace with your API URL
+
+export default API_URL;
---
+++ frontend/src/api/auth.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import API_URL from "./apiConfig";
+
+export const login = async (credentials) => {
+  try {
+    const response = await axios.post(`${API_URL}/login`, credentials);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const register = async (userData) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, userData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
