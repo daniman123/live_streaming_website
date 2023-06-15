79a1615e9ee5df9e74d0be329eb288ed02d5d8f0
daniman123 <daniel-saleh@hotmail.com>
2023-06-15 19:23:36 +0200

    ss

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/", authRoutes);
-app.use("/", userDataRoutes);
+// app.use("/", userDataRoutes);
 
 app.get("/recommended", async (req, res) => {
   const {
