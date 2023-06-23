dbb00f39c80a6ffed0b8882dbd6ce035c69fd934
daniman123 <daniel-saleh@hotmail.com>
2023-06-23 14:42:32 +0200

    sdfgfd

--- client/src/features/logo/index.tsx
+++ client/src/features/logo/index.tsx
@@ -4,10 +4,8 @@ import Link from "next/link";
 const Logo: React.FC = () => {
   return (
     <div className="logo__wrapper">
-      <Link href="/" passHref>
-        <a className="hidden-link">
-          <div className="logo">LOGO</div>
-        </a>
+      <Link href="/" className="hidden-link">
+        <div className="logo">LOGO</div>
       </Link>
     </div>
   );
