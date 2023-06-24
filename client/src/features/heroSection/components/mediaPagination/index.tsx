5319166aeb24c30d05cc8a8bc3ce4ac3e5d79f54
daniman123 <>
2023-06-24 03:32:47 +0200

    ss

--- client/src/features/heroSection/components/mediaPagination/index.tsx
+++ client/src/features/heroSection/components/mediaPagination/index.tsx
@@ -1,79 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 
-type Props = {};
-
-interface MetaData {
-  user: string;
+interface Media {
+  image: string;
+  title: string;
   type: string;
-  views: number;
-  likes: number;
-  dislikes: number;
-  numOfComments: number;
 }
 
-function MediaPagination({}: Props) {
-  const media: MetaData[] = [
+const HeroSection: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const mediaPerPage = 3;
+  const media: Media[] = [
     {
-      user: "dani",
-      type: "text",
-      views: 100000,
-      likes: 10000,
-      dislikes: 19999,
-      numOfComments: 170,
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Live Stream Channel 1",
+      type: "channel",
     },
     {
-      user: "phil",
-      type: "video",
-      views: 100000,
-      likes: 10000,
-      dislikes: 19999,
-      numOfComments: 170,
-    },
-    {
-      user: "fam",
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Clip/Short Video 13",
       type: "clip",
-      views: 100000,
-      likes: 10000,
-      dislikes: 19999,
-      numOfComments: 170,
     },
     {
-      user: "gabb",
-      type: "peek",
-      views: 100000,
-      likes: 10000,
-      dislikes: 19999,
-      numOfComments: 170,
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Community Post 14",
+      type: "post",
+    },
+    {
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Community Post 21",
+      type: "post",
+    },
+    {
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Community Post 51",
+      type: "post",
+    },
+    {
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Community Post 51",
+      type: "post",
+    },
+    {
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Community Post 531",
+      type: "post",
+    },
+    {
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Community Post 15",
+      type: "post",
+    },
+    {
+      image:
+        "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_6436.jpg",
+      title: "Community Post 71",
+      type: "post",
     },
   ];
 
-  const displayData = () => {
-    return media.map((item, index: number) => {
-      return (
-        <div key={index} className="media__pagination__item">
-          {Object.entries(item).map(([dataType, data]) => {
-            return (
-              <div
-                key={data + dataType}
-                className="media__pagination__item__data"
-              >
-                {dataType}
-                {" "}
-                {data}
-              </div>
-            );
-          })}
-        </div>
-      );
-    });
+  const numSlides = Math.ceil(media.length / mediaPerPage);
+
+  const goToNextSlide = (): void => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % numSlides);
   };
 
+  const goToPreviousSlide = (): void => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + numSlides) % numSlides);
+  };
+
+  const startMediaIndex = currentSlide * mediaPerPage;
+  const visibleMedia = media.slice(
+    startMediaIndex,
+    startMediaIndex + mediaPerPage
+  );
+
   return (
-    <div className="home__media__pagination">
-      MediaPagination
-      <div className="media__wrapper">{displayData()}</div>
+    <div className="hero-section">
+      <div className="slideshow">
+        {visibleMedia.map((mediaItem, index) => (
+          <Slide key={index} media={mediaItem} />
+        ))}
+      </div>
+
+      <div className="controls">
+        <button onClick={goToPreviousSlide}>Previous</button>
+        <button onClick={goToNextSlide}>Next</button>
+      </div>
     </div>
   );
+};
+
+interface SlideProps {
+  media: Media;
 }
 
-export default MediaPagination;
+const Slide: React.FC<SlideProps> = ({ media }) => {
+  const { image, title, type } = media;
+
+  return (
+    <div className="slide">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{type}</p>
+    </div>
+  );
+};
+
+export default HeroSection;
